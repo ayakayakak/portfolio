580b02151dedf95b9fee8db3be9566e52d683ec3
ayakayakak <bayakau.tka@gmail.com>
2024-10-04 18:05:37 +0900

    fix: card 5 disabled condition

--- frontend/components/molecules/tagiron/number-cards.tsx
+++ frontend/components/molecules/tagiron/number-cards.tsx
@@ -6,7 +6,7 @@ import styled from 'styled-components'
 import { TagironNumberCard } from 'components/atoms'
 
 /* types */
-import { NumberCardType } from 'types/myTypes'
+import { NumberCardType, CardColor } from 'types/myTypes'
 
 /* const */
 import { BLUE_NUMBER_CARDS, RED_NUMBER_CARDS } from 'const'
@@ -29,12 +29,13 @@ const StyledTagironNumberCards = styled.div`
 `
 
 export const TagironNumberCards: NextComponentType<NextPageContext, null, Props> = ({ className = '', disabledCards, onClick }) => {
-  const isDisabledCards = (card: NumberCardType): boolean => {
+  const isDisabledCards = (card: NumberCardType, cardColor: CardColor): boolean => {
     if (card.number === 5) {
-      // 5は同じ色で2枚あるので、disabledCardsから被りを消去してlengthが減るかでdisabled判定
-      const disabledCardsWithoutUndefined = disabledCards.filter((card) => card !== undefined)
-      const disabledCardsWithoutDuplicationAndUndefined = Array.from(new Set(disabledCardsWithoutUndefined))
-      return disabledCardsWithoutUndefined.length !== disabledCardsWithoutDuplicationAndUndefined.length
+      // 5は同じ色で2枚あるので、青の中の5から順にdisabledにする
+      const numberOf5Cards = disabledCards.filter((disabledCard) => disabledCard?.number === 5).length
+      if (numberOf5Cards === 1 && cardColor === 'blue') return true
+      if (numberOf5Cards === 2) return true
+      return false
     }
 
     return disabledCards.some((disabledCard) => disabledCard?.color === card.color && disabledCard?.number === card.number)
@@ -44,12 +45,24 @@ export const TagironNumberCards: NextComponentType<NextPageContext, null, Props>
     <StyledTagironNumberCards className={className}>
       <div className="row">
         {BLUE_NUMBER_CARDS.map((card, index) => (
-          <TagironNumberCard cardType={card} onClick={onClick} disabled={isDisabledCards(card)} key={index} className="number-card" />
+          <TagironNumberCard
+            cardType={card}
+            onClick={onClick}
+            disabled={isDisabledCards(card, 'blue')}
+            key={index}
+            className="number-card"
+          />
         ))}
       </div>
       <div className="row">
         {RED_NUMBER_CARDS.map((card, index) => (
-          <TagironNumberCard cardType={card} onClick={onClick} disabled={isDisabledCards(card)} key={index} className="number-card" />
+          <TagironNumberCard
+            cardType={card}
+            onClick={onClick}
+            disabled={isDisabledCards(card, 'red')}
+            key={index}
+            className="number-card"
+          />
         ))}
       </div>
     </StyledTagironNumberCards>
--- frontend/types/myTypes.ts
+++ frontend/types/myTypes.ts
@@ -3,4 +3,5 @@ export type SectionList = 'ABOUT' | 'SKILL' | 'CONTACT'
 export type FlashMessageType = 'success' | 'fail'
 
 /* tagiron */
-export type NumberCardType = { color: 'blue' | 'red' | 'yellow'; number: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 }
+export type NumberCardType = { color: CardColor; number: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 }
+export type CardColor = 'blue' | 'red' | 'yellow'
